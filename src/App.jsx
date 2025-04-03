784f322874e1b3c89f710abde6fdf1359af23c44
Florian SIMUNEK <60973568+floriansimunek@users.noreply.github.com>
2025-04-03 22:48:59 +0200

    ✨ Add image credit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import Card from './components/Card';
 
 function App() {
-  return <Card />;
+  return (
+    <>
+      <Card />
+      <div className="image-credit">
+        Image by&nbsp;
+        <a href="https://unsplash.com/fr/photos/batiment-en-ciment-moderne-blanc-sous-le-ciel-bleu-RFDP7_80v5A" target="_blank">
+          Joel Filipe
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default App;
